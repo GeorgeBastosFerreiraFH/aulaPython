fe66947ece95a49f24d43e646477c217fbe2f2c6
GeorgeBastosFerreiraFH <114554305+GeorgeBastosFerreiraFH@users.noreply.github.com>
2024-10-28 18:46:32 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,65 @@
+# set1 = {'Infinity', 'School', '202...'}
+
+# set2 = {letra for letra in 'infinity' if letra not in 'aeiou'}
+
+# set1 = set(['Infinity', 'School', '202...'])
+
+# print(set1)
+# print(set2)
+
+# frutas = {''}
+
+# frutas.add('maça')
+# frutas.add('banana')
+# frutas.add('uva')
+# frutas.add('laranja')
+# frutas.add('morango')
+
+# print(frutas)
+
+# frutaEscolhida = input('Digite um fruta: ')
+
+# resulado = frutaEscolhida.intersection
+
+# if frutaEscolhida in frutas:
+#     print(f'Essa {frutaEscolhida} está na lista')
+# else:
+#     print('Fruta não encontrada')
+
+
+# # Atividade 3 e 4
+
+# frutas_vermelhas = set()
+
+# frutas_vermelhas.add('morango')
+# frutas_vermelhas.add('cereja')
+# frutas_vermelhas.add('framboesa')
+
+# print(frutas_vermelhas)
+
+# frutas_vermelhas.remove('cereja')
+# print(frutas_vermelhas)
+
+# # Atividade 5
+
+# conjunto_A = {'A', 'C', 'E', 'G', 'I'}
+# conjunto_B = {'B', 'D', 'F', 'H', 'J'}
+
+# print(conjunto_A.union(conjunto_B))
+
+# # Atividade 6
+
+# conjunto_um = {1, 2, 3, 4, 5}
+# conjunto_dois = {4, 5, 6, 7, 8}
+
+# print(conjunto_um.intersection(conjunto_dois))
+
+# # Atividade 7
+
+# lista1 = set(input("Digite os elementos da primeira lista, separados por vírgula: ").split(','))
+# lista2 = set(input("Digite os elementos da segunda lista, separados por vírgula: ").split(','))
+
+# uniao = lista1 | lista2
+
+# print(f"A união dos elementos únicos das listas é: {uniao}")
+
